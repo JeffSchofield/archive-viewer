d76e63b49bf3c99d6eba9ef616c9db3a9f078b09
Jeff Schofield <jeff@jeffschofield.com>
2022-02-19 19:19:12 -0500

    feat: Add icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import { colorSuitePlugin } from 'tailwindcss-color-suite'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 export default defineConfig({
   plugins: [
     vue(),
     colorSuitePlugin(),
+    createSvgIconsPlugin({
+      iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
+      customDomId: 'svg-icons'
+    })
   ],
   publicDir: 'swf',
   envPrefix: 'VIEWER_'
