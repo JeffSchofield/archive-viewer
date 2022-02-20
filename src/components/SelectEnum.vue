23a105790a7de5158db5eb67beaab54a6b50d85b
Jeff Schofield <jeff@jeffschofield.com>
2022-02-19 19:21:24 -0500

    refactor: Add chevron back to drop down

--- src/components/SelectEnum.vue
+++ src/components/SelectEnum.vue
@@ -35,7 +35,7 @@
   <Listbox as="div" v-model="selected" class="relative flex not-prose" v-slot="{ open }">
     <ListboxButton class="flex-1 flex items-center rounded-3/8 px-1/2 py-1/4 text-left text-black dark:text-white border border-neutral-200 dark:border-0 bg-neutral-50 dark:bg-neutral-750" :class="{ 'rounded-b-none border-b-0': open }">
 			<div class="flex-1 leading-1">{{ selected[0] ? formatValue(selected[0]) : 'Select...' }}</div>
-			<!-- <SvgIcon name="chevron-down" class="h-3/4 fill-current" :class="{'rotate-180':open}" /> -->
+			<SvgIcon name="chevron-down" class="h-3/4 fill-current" :class="{'rotate-180':open}" />
 		</ListboxButton>
     <ListboxOptions class="absolute top-100% inset-x-0 p-0 z-popover max-h-14 overflow-y-auto border border-neutral-200 dark:border-0 dark:border-t border-t-neutral-300 dark:border-t-neutral-850 rounded-b-3/8 shadow-sm">
 			<ListboxOption
